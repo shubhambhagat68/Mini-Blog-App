14d7c3d2ea5eb2dde18878ad6318765050d47d68
Shubham Bhagat <bhagatshubham68@gmail.com>
2021-07-27 03:04:26 +0530

    Moderation file added and changes made to maintain the data flow

--- query/index.js
+++ query/index.js
@@ -15,7 +15,7 @@ app.get('/posts',(req,res)=>{
 
 app.post('/events',(req,res)=>{
 	const {type,data}= req.body
-
+ 
 	if(type==='PostCreated'){
 		const {id,title}=data
 
@@ -24,11 +24,23 @@ app.post('/events',(req,res)=>{
 
 	if(type==='CommentCreated'){
 
-		const {id,content,postId}=data
+		const {id,content,postId,status}=data
 
 		const post=posts[postId]
-		post.comments.push({id,content})
+		post.comments.push({id,content,status})
 
+	}
+
+	if(type==='CommentUpdated'){
+		const {id,content,postId,status}=data
+
+		const post=posts[postId]
+		const comment =post.comment.find(comment=>{
+			return comment.id==id
+		})
+
+		comment.status=status
+		comment.content=content
 	}
 
 
